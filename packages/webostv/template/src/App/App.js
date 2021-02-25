9cc2f73ecb6991fd2e0bcc3f6794f63bb21babdd
Mikyung Kim <mikyung27.kim@lge.com>
2021-02-25 15:39:30 +0900

    Remove sandstone related code for release
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- packages/webostv/template/src/App/App.js
+++ packages/webostv/template/src/App/App.js
@@ -1,6 +1,6 @@
 import kind from '@enact/core/kind';
-import ThemeDecorator from '@enact/sandstone/ThemeDecorator';
-import Panels from '@enact/sandstone/Panels';
+import ThemeDecorator from '@enact/moonstone/ThemeDecorator';
+import Panels from '@enact/moonstone/Panels';
 import React from 'react';
 
 import MainPanel from '../views/MainPanel';
--- packages/webostv/template/src/views/MainPanel.js
+++ packages/webostv/template/src/views/MainPanel.js
@@ -1,6 +1,6 @@
-import Button from '@enact/sandstone/Button';
+import Button from '@enact/moonstone/Button';
 import kind from '@enact/core/kind';
-import {Panel, Header} from '@enact/sandstone/Panels';
+import {Panel, Header} from '@enact/moonstone/Panels';
 import React from 'react';
 
 const MainPanel = kind({
