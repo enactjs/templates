b90cbddc6802e236844ddab18605f1b4ce0b7db6
taeyoung.hong <taeyoung.hong@lge.com>
2022-04-26 16:39:51 +0900

    remove CRA link

--- packages/sandstone/template/src/index.js
+++ packages/sandstone/template/src/index.js
@@ -16,5 +16,5 @@ export default appElement;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint.
 reportWebVitals();
