4396e755c351651ed626bb846c3adadf08c35d19
Mikyung Kim <mikyung27.kim@lge.com>
2022-06-23 14:39:04 +0900

    revert
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- packages/typescript/template/src/index.tsx
+++ packages/typescript/template/src/index.tsx
@@ -1,5 +1,5 @@
-/* global ENACT_PACK_ISOMORPHIC */
-import {createRoot, hydrateRoot} from 'react-dom/client';
+import {createRoot} from 'react-dom/client'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -7,11 +7,10 @@ const appElement = (<App />);
 
 // In a browser environment, render instead of exporting
 if (typeof window !== 'undefined') {
-    if (ENACT_PACK_ISOMORPHIC) {
-        hydrateRoot(document.getElementById('root'), appElement);
-    } else {
-        createRoot(document.getElementById('root')).render(appElement);
-    }
+	const container = document.getElementById('root')!;
+	const root = createRoot(container);
+
+	root.render(appElement);
 }
 
 export default appElement;
