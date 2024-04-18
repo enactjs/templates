9119404116dd06307734730becbff07b2980d86a
Daniel Stoian <daniel.stoian@lgepartner.com>
2024-04-18 16:15:50 +0300

    replaced FID with INP in web-vitals

--- packages/typescript/template/src/reportWebVitals.ts
+++ packages/typescript/template/src/reportWebVitals.ts
@@ -2,12 +2,12 @@ import {ReportHandler} from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
+		import('web-vitals').then(({getCLS, getFCP, getLCP, getTTFB, onINP}) => {
 			getCLS(onPerfEntry);
-			getFID(onPerfEntry);
 			getFCP(onPerfEntry);
 			getLCP(onPerfEntry);
 			getTTFB(onPerfEntry);
+			onINP(onPerfEntry);
 		});
 	}
 };
--- packages/webosauto/template/src/reportWebVitals.js
+++ packages/webosauto/template/src/reportWebVitals.js
@@ -1,11 +1,11 @@
 const reportWebVitals = (onPerfEntry) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
+		import('web-vitals').then(({getCLS, getFCP, getLCP, getTTFB, onINP}) => {
 			getCLS(onPerfEntry);
-			getFID(onPerfEntry);
 			getFCP(onPerfEntry);
 			getLCP(onPerfEntry);
 			getTTFB(onPerfEntry);
+			onINP(onPerfEntry);
 		});
 	}
 };
