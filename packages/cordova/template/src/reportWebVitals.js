3c8632b5983612812b2f6dd4b5413fb990dcc6d1
Daniel Stoian <daniel.stoian@lgepartner.com>
2024-04-19 09:37:50 +0300

    reverted removal of FID

--- packages/cordova/template/src/reportWebVitals.js
+++ packages/cordova/template/src/reportWebVitals.js
@@ -1,7 +1,8 @@
 const reportWebVitals = (onPerfEntry) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals').then(({getCLS, getFCP, getLCP, getTTFB, onINP}) => {
+		import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB, onINP}) => {
 			getCLS(onPerfEntry);
+			getFID(onPerfEntry);
 			getFCP(onPerfEntry);
 			getLCP(onPerfEntry);
 			getTTFB(onPerfEntry);
--- packages/typescript/template/src/reportWebVitals.ts
+++ packages/typescript/template/src/reportWebVitals.ts
@@ -2,8 +2,9 @@ import {ReportHandler} from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals').then(({getCLS, getFCP, getLCP, getTTFB, onINP}) => {
+		import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB, onINP}) => {
 			getCLS(onPerfEntry);
+			getFID(onPerfEntry);
 			getFCP(onPerfEntry);
 			getLCP(onPerfEntry);
 			getTTFB(onPerfEntry);
