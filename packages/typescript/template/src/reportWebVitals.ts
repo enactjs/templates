809d093cebd58e82fb37eac670e2c98a50d06d25
Daniel Stoian <daniel.stoian@lgepartner.com>
2024-04-19 13:13:47 +0300

    replaced onINP with getINP

--- packages/typescript/template/src/reportWebVitals.ts
+++ packages/typescript/template/src/reportWebVitals.ts
@@ -2,13 +2,13 @@ import {ReportHandler} from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB, onINP}) => {
+		import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB, getINP}) => {
 			getCLS(onPerfEntry);
 			getFID(onPerfEntry);
 			getFCP(onPerfEntry);
 			getLCP(onPerfEntry);
 			getTTFB(onPerfEntry);
-			onINP(onPerfEntry);
+			getINP(onPerfEntry);
 		});
 	}
 };
--- packages/webosauto/template/src/reportWebVitals.js
+++ packages/webosauto/template/src/reportWebVitals.js
@@ -1,12 +1,12 @@
 const reportWebVitals = (onPerfEntry) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB, onINP}) => {
+		import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB, getINP}) => {
 			getCLS(onPerfEntry);
 			getFID(onPerfEntry);
 			getFCP(onPerfEntry);
 			getLCP(onPerfEntry);
 			getTTFB(onPerfEntry);
-			onINP(onPerfEntry);
+			getINP(onPerfEntry);
 		});
 	}
 };
