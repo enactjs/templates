b19ffe50b6aed2de77191c54b470cd64e85562f5
Paul Beldean <paul.beldean@lgepartner.com>
2024-07-04 17:05:46 +0300

    fixed review issues

--- packages/cordova/template/src/views/MainPanel.js
+++ packages/cordova/template/src/views/MainPanel.js
@@ -1,6 +1,6 @@
-import Button from '@enact/sandstone/Button';
 import kind from '@enact/core/kind';
-import {Panel, Header} from '@enact/sandstone/Panels';
+import Button from '@enact/sandstone/Button';
+import {Header, Panel} from '@enact/sandstone/Panels';
 
 const MainPanel = kind({
 	name: 'MainPanel',
