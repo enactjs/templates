14a59cda1864b0dd2179a161a4a32f585bec134f
Paul Beldean <paul.beldean@lgepartner.com>
2024-07-22 16:48:50 +0300

    added onINP and updated moonstone

--- packages/sandstone/template/src/reportWebVitals.js
+++ packages/sandstone/template/src/reportWebVitals.js
@@ -1,9 +1,10 @@
 const reportWebVitals = (onPerfEntry) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals').then(({onCLS, onFID, onFCP, onLCP, onTTFB}) => {
+		import('web-vitals').then(({onCLS, onFID, onFCP, onINP, onLCP, onTTFB}) => {
 			onCLS(onPerfEntry);
 			onFID(onPerfEntry);
 			onFCP(onPerfEntry);
+			onINP(onPerfEntry);
 			onLCP(onPerfEntry);
 			onTTFB(onPerfEntry);
 		});
--- packages/typescript/template/src/reportWebVitals.ts
+++ packages/typescript/template/src/reportWebVitals.ts
@@ -2,10 +2,11 @@ import {MetricType} from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals').then(({onCLS, onFID, onFCP, onLCP, onTTFB}) => {
+		import('web-vitals').then(({onCLS, onFID, onFCP, onINP, onLCP, onTTFB}) => {
 			onCLS(onPerfEntry);
 			onFID(onPerfEntry);
 			onFCP(onPerfEntry);
+			onINP(onPerfEntry);
 			onLCP(onPerfEntry);
 			onTTFB(onPerfEntry);
 		});
