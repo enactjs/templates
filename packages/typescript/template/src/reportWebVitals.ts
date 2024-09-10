4c0e45e6e6af368b1969af68db9273977b8841fb
Daniel Stoian <daniel.stoian@lgepartner.com>
2024-09-10 16:27:21 +0300

    removed onFID from web-vitals call

--- packages/typescript/template/src/reportWebVitals.ts
+++ packages/typescript/template/src/reportWebVitals.ts
@@ -2,9 +2,8 @@ import {MetricType} from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import('web-vitals').then(({onCLS, onFID, onFCP, onINP, onLCP, onTTFB}) => {
+		import('web-vitals').then(({onCLS, onFCP, onINP, onLCP, onTTFB}) => {
 			onCLS(onPerfEntry);
-			onFID(onPerfEntry);
 			onFCP(onPerfEntry);
 			onINP(onPerfEntry);
 			onLCP(onPerfEntry);
