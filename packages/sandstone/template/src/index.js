0e2ee4189a3ef1b79ba98abb21c10413e2849c2d
Daniel Stoian <daniel.stoian@lgepartner.com>
2024-10-29 16:02:06 +0200

    test travis

--- packages/sandstone/template/src/index.js
+++ packages/sandstone/template/src/index.js
@@ -21,4 +21,4 @@ export default appElement;
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint.
 // Learn more: https://github.com/enactjs/cli/blob/master/docs/measuring-performance.md
-reportWebVitals();
+reportWebVitals(console.log);
